4728601433a0fa3f8c6eb21112a5371b3607a363
Monali <167152583+Monali-0830@users.noreply.github.com>
2025-05-24 18:29:57 +0530

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -12,7 +12,12 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("✅ Connected to MongoDB"))
+.then(() => {
+    console.log("✅ Connected to MongoDB");
+    app.listen(333, () => {
+        console.log("Server is Running on port 333");
+    });
+})
 .catch(err => console.error("❌ MongoDB connection error:", err));
 
 app.use('/reminder', reminderroute); 
@@ -21,6 +26,4 @@ app.get('/', (req, res) => {
     res.send("Hello from 333");
 });
 
-app.listen(333, () => {
-    console.log("Server is Running on port 333");
-});
+
